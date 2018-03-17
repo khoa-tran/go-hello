c5237333a20a2e12872ecb47567c95a08d573fab
Khoa Tran <akhoatr@gmail.com>
2018-03-16 17:11:19 -0700

    clean up

--- array/sum.go
+++ array/sum.go
@@ -1,9 +1,30 @@
 package sum
 
 // Sum takes an array of numbers and returns sum
-func Sum(numbers [5]int) (sum int) {
+func Sum(numbers []int) (sum int) {
 	for _, number := range numbers {
 		sum += number
+	}
+	return
+}
+
+// SumAll takes varying number of slices and return new slice containing the sums of each slice
+func SumAll(slices ...[]int) (sums []int) {
+	for _, numbers := range slices {
+		sums = append(sums, Sum(numbers))
+	}
+	return
+}
+
+// SumAllTails takes varying number of slices and return new slice containing the sum of tails of each slice
+func SumAllTails(slices ...[]int) (sums []int) {
+	for _, numbers := range slices {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 	return
 }
